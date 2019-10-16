a2c5202d2982edcc64935a57b91a4c23000635e1
Raymundo González Luna <44013508+RaymundoGonzalezL@users.noreply.github.com>
2019-10-16 12:20:11 -0500

    Update C.cpp
    Accepted

--- Fecha 1/C.cpp
+++ Fecha 1/C.cpp
@@ -13,45 +13,53 @@ int findSet(int i){
     return (p[i] == i) ? i : (p[i] = findSet(p[i]));
 }
 
-bool isSameSet(int i, int j){
-    return findSet(i) == findSet(j);
-}
-
 void unionSet(int i, int j){
     int x = findSet(i), y = findSet(j);
-    if(!isSameSet(x,y)){
+    if(x != y){
         S--;
-        p[y] = x;
+        p[x] = y;
     }
 }
+typedef pair<int,int> ii;
+typedef pair<int, ii> iii;
+iii V[100005];
 
 int main(){
     int T, N, M, u, v, c;
-    cin >> T;
+    scanf("%d", &T);
     while(T--){
-        cin >> N >> M;
+        scanf("%d %d", &N, &M);
         initSet(N);
         for(int i=0;i<M;i++){
-            cin >> u >> v >> c;
+            scanf("%d %d %d", &u, &v, &c);
             --u, --v;
             unionSet(u, v);
         }
-        bool ok = (S <= 2);
         int R;
-        int mini = INT_MAX;
-        cin >> R;
-        while(R--){
-            cin >> u >> v >> c;
-            if(ok){
-                --u, --v;
-                if(!isSameSet(u, v)) mini = min(mini, c);
+        scanf("%d", &R);
+        for(int i=0;i<R;i++){
+            scanf("%d %d %d", &u, &v, &c);
+            --u, --v;
+            V[i] = (iii(c,ii(u, v)));
+        }
+        if(S == 1){
+            printf("Thank you, Goodbye\n");
+            continue;
+        }
+        long long ans = 0;
+        sort(V, V + R);
+        for(int i=0;i<R;i++){
+            int x = findSet(V[i].second.first), y = findSet(V[i].second.second);
+            if(x != y){
+                unionSet(x, y);
+                ans+= V[i].first;
+                if(S == 1) break;
             }
         }
-        if(!ok)
-            cout << "Thank you, Goodbye\n";
-        else if(mini == INT_MAX)
-            cout << "You better hire someone else\n";
-        else
-            cout << mini << "\n";
+        if(S > 1){
+            printf("You better hire someone else\n");
+        }else{
+            printf("%lld\n", ans);
+        }
     }
 }
